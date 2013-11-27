e2921cd0a9ea82ffc9df09ddf66287ed7f8dc971
Flo <florian@kletz.de>
2013-11-27 13:01:54 +0100

    bugfix

--- CI_Aufgabe3/teil1/symbolraetsel_AST_Normalisiert/output/SymbolraetselASTNormalizer.java
+++ CI_Aufgabe3/teil1/symbolraetsel_AST_Normalisiert/output/SymbolraetselASTNormalizer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.3 Nov 30, 2010 12:45:30 C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g 2013-11-25 12:59:46
+// $ANTLR 3.3 Nov 30, 2010 12:45:30 C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g 2013-11-27 12:40:51
  package symbolraetsel_AST_Normalisiert.output;
 
 import org.antlr.runtime.*;
@@ -6,8 +6,10 @@ import org.antlr.runtime.tree.*;import java.util.Stack;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.antlr.runtime.debug.*;
+import java.io.IOException;
 
-public class SymbolraetselASTNormalizer extends TreeParser {
+public class SymbolraetselASTNormalizer extends DebugTreeParser {
     public static final String[] tokenNames = new String[] {
         "<invalid>", "<EOR>", "<DOWN>", "<UP>", "PLUS", "MINUS", "LETTER", "EQUALS", "SPACE", "LINEBREAK", "ADDITION"
     };
@@ -23,23 +25,60 @@ public class SymbolraetselASTNormalizer extends TreeParser {
     // delegates
     // delegators
 
+    public static final String[] ruleNames = new String[] {
+        "invalidRule", "arith", "redraw", "start"
+    };
+    public static final boolean[] decisionCanBacktrack = new boolean[] {
+        false, // invalid decision
+        false, false
+    };
 
+     
+        public int ruleLevel = 0;
+        public int getRuleLevel() { return ruleLevel; }
+        public void incRuleLevel() { ruleLevel++; }
+        public void decRuleLevel() { ruleLevel--; }
         public SymbolraetselASTNormalizer(TreeNodeStream input) {
-            this(input, new RecognizerSharedState());
+            this(input, DebugEventSocketProxy.DEFAULT_DEBUGGER_PORT, new RecognizerSharedState());
         }
-        public SymbolraetselASTNormalizer(TreeNodeStream input, RecognizerSharedState state) {
+        public SymbolraetselASTNormalizer(TreeNodeStream input, int port, RecognizerSharedState state) {
             super(input, state);
-             
+            DebugEventSocketProxy proxy =
+                new DebugEventSocketProxy(this,port,input.getTreeAdaptor());
+            setDebugListener(proxy);
+            setTreeNodeStream(new DebugTreeNodeStream(input,proxy));
+            try {
+                proxy.handshake();
+            }
+            catch (IOException ioe) {
+                reportError(ioe);
+            }
+            TreeAdaptor adap = new CommonTreeAdaptor();
+            setTreeAdaptor(adap);
+            proxy.setTreeAdaptor(adap);
         }
-        
-    protected TreeAdaptor adaptor = new CommonTreeAdaptor();
+    public SymbolraetselASTNormalizer(TreeNodeStream input, DebugEventListener dbg) {
+        super(input, dbg);
 
+         
+        TreeAdaptor adap = new CommonTreeAdaptor();
+        setTreeAdaptor(adap);
+
+    }
+    protected boolean evalPredicate(boolean result, String predicate) {
+        dbg.semanticPredicate(result, predicate);
+        return result;
+    }
+
+    protected DebugTreeAdaptor adaptor;
     public void setTreeAdaptor(TreeAdaptor adaptor) {
-        this.adaptor = adaptor;
+        this.adaptor = new DebugTreeAdaptor(dbg,adaptor);
+
     }
     public TreeAdaptor getTreeAdaptor() {
         return adaptor;
     }
+
 
     public String[] getTokenNames() { return SymbolraetselASTNormalizer.tokenNames; }
     public String getGrammarFileName() { return "C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g"; }
@@ -65,17 +104,29 @@ public class SymbolraetselASTNormalizer extends TreeParser {
 
 
 
+        try { dbg.enterRule(getGrammarFileName(), "start");
+        if ( getRuleLevel()==0 ) {dbg.commence();}
+        incRuleLevel();
+        dbg.location(18, 1);
+
         try {
             // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:18:7: ( ( redraw )+ )
+            dbg.enterAlt(1);
+
             // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:18:9: ( redraw )+
             {
             root_0 = (CommonTree)adaptor.nil();
 
+            dbg.location(18,9);
             // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:18:9: ( redraw )+
             int cnt1=0;
+            try { dbg.enterSubRule(1);
+
             loop1:
             do {
                 int alt1=2;
+                try { dbg.enterDecision(1, decisionCanBacktrack[1]);
+
                 int LA1_0 = input.LA(1);
 
                 if ( (LA1_0==EQUALS) ) {
@@ -83,10 +134,15 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                 }
 
 
+                } finally {dbg.exitDecision(1);}
+
                 switch (alt1) {
             	case 1 :
+            	    dbg.enterAlt(1);
+
             	    // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:18:9: redraw
             	    {
+            	    dbg.location(18,9);
             	    _last = (CommonTree)input.LT(1);
             	    pushFollow(FOLLOW_redraw_in_start65);
             	    redraw1=redraw();
@@ -102,10 +158,13 @@ public class SymbolraetselASTNormalizer extends TreeParser {
             	    if ( cnt1 >= 1 ) break loop1;
                         EarlyExitException eee =
                             new EarlyExitException(1, input);
+                        dbg.recognitionException(eee);
+
                         throw eee;
                 }
                 cnt1++;
             } while (true);
+            } finally {dbg.exitSubRule(1);}
 
 
             }
@@ -119,6 +178,15 @@ public class SymbolraetselASTNormalizer extends TreeParser {
         }
         finally {
         }
+        dbg.location(18, 16);
+
+        }
+        finally {
+            dbg.exitRule(getGrammarFileName(), "start");
+            decRuleLevel();
+            if ( getRuleLevel()==0 ) {dbg.terminate();}
+        }
+
         return retval;
     }
     // $ANTLR end "start"
@@ -159,9 +227,16 @@ public class SymbolraetselASTNormalizer extends TreeParser {
         RewriteRuleNodeStream stream_MINUS=new RewriteRuleNodeStream(adaptor,"token MINUS");
         RewriteRuleNodeStream stream_LETTER=new RewriteRuleNodeStream(adaptor,"token LETTER");
 
+        try { dbg.enterRule(getGrammarFileName(), "redraw");
+        if ( getRuleLevel()==0 ) {dbg.commence();}
+        incRuleLevel();
+        dbg.location(20, 1);
+
         try {
             // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:20:7: ( ^( EQUALS ^( PLUS a= LETTER b= LETTER ) c= LETTER ) -> ^( EQUALS ^( ADDITION $a $b) $c) | ^( EQUALS ^( MINUS a= LETTER b= LETTER ) c= LETTER ) -> ^( EQUALS ^( ADDITION $c $b) $a) )
             int alt2=2;
+            try { dbg.enterDecision(2, decisionCanBacktrack[2]);
+
             int LA2_0 = input.LA(1);
 
             if ( (LA2_0==EQUALS) ) {
@@ -180,6 +255,7 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                         NoViableAltException nvae =
                             new NoViableAltException("", 2, 2, input);
 
+                        dbg.recognitionException(nvae);
                         throw nvae;
                     }
                 }
@@ -187,6 +263,7 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                     NoViableAltException nvae =
                         new NoViableAltException("", 2, 1, input);
 
+                    dbg.recognitionException(nvae);
                     throw nvae;
                 }
             }
@@ -194,38 +271,49 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                 NoViableAltException nvae =
                     new NoViableAltException("", 2, 0, input);
 
+                dbg.recognitionException(nvae);
                 throw nvae;
             }
+            } finally {dbg.exitDecision(2);}
+
             switch (alt2) {
                 case 1 :
+                    dbg.enterAlt(1);
+
                     // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:20:10: ^( EQUALS ^( PLUS a= LETTER b= LETTER ) c= LETTER )
                     {
+                    dbg.location(20,10);
                     _last = (CommonTree)input.LT(1);
                     {
                     CommonTree _save_last_1 = _last;
                     CommonTree _first_1 = null;
-                    CommonTree root_1 = (CommonTree)adaptor.nil();_last = (CommonTree)input.LT(1);
+                    CommonTree root_1 = (CommonTree)adaptor.nil();dbg.location(20,12);
+                    _last = (CommonTree)input.LT(1);
                     EQUALS2=(CommonTree)match(input,EQUALS,FOLLOW_EQUALS_in_redraw75);  
                     stream_EQUALS.add(EQUALS2);
 
 
 
                     match(input, Token.DOWN, null); 
+                    dbg.location(20,19);
                     _last = (CommonTree)input.LT(1);
                     {
                     CommonTree _save_last_2 = _last;
                     CommonTree _first_2 = null;
-                    CommonTree root_2 = (CommonTree)adaptor.nil();_last = (CommonTree)input.LT(1);
+                    CommonTree root_2 = (CommonTree)adaptor.nil();dbg.location(20,21);
+                    _last = (CommonTree)input.LT(1);
                     PLUS3=(CommonTree)match(input,PLUS,FOLLOW_PLUS_in_redraw78);  
                     stream_PLUS.add(PLUS3);
 
 
 
                     match(input, Token.DOWN, null); 
+                    dbg.location(20,27);
                     _last = (CommonTree)input.LT(1);
                     a=(CommonTree)match(input,LETTER,FOLLOW_LETTER_in_redraw82);  
                     stream_LETTER.add(a);
 
+                    dbg.location(20,36);
                     _last = (CommonTree)input.LT(1);
                     b=(CommonTree)match(input,LETTER,FOLLOW_LETTER_in_redraw86);  
                     stream_LETTER.add(b);
@@ -234,6 +322,7 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                     match(input, Token.UP, null); adaptor.addChild(root_1, root_2);_last = _save_last_2;
                     }
 
+                    dbg.location(20,46);
                     _last = (CommonTree)input.LT(1);
                     c=(CommonTree)match(input,LETTER,FOLLOW_LETTER_in_redraw91);  
                     stream_LETTER.add(c);
@@ -245,7 +334,7 @@ public class SymbolraetselASTNormalizer extends TreeParser {
 
 
                     // AST REWRITE
-                    // elements: b, EQUALS, c, a
+                    // elements: a, EQUALS, b, c
                     // token labels: b, c, a
                     // rule labels: retval
                     // token list labels: 
@@ -260,21 +349,28 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                     root_0 = (CommonTree)adaptor.nil();
                     // 21:4: -> ^( EQUALS ^( ADDITION $a $b) $c)
                     {
+                        dbg.location(21,7);
                         // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:21:7: ^( EQUALS ^( ADDITION $a $b) $c)
                         {
                         CommonTree root_1 = (CommonTree)adaptor.nil();
+                        dbg.location(21,9);
                         root_1 = (CommonTree)adaptor.becomeRoot(stream_EQUALS.nextNode(), root_1);
 
+                        dbg.location(21,16);
                         // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:21:16: ^( ADDITION $a $b)
                         {
                         CommonTree root_2 = (CommonTree)adaptor.nil();
+                        dbg.location(21,18);
                         root_2 = (CommonTree)adaptor.becomeRoot((CommonTree)adaptor.create(ADDITION, "ADDITION"), root_2);
 
+                        dbg.location(21,27);
                         adaptor.addChild(root_2, stream_a.nextNode());
+                        dbg.location(21,30);
                         adaptor.addChild(root_2, stream_b.nextNode());
 
                         adaptor.addChild(root_1, root_2);
                         }
+                        dbg.location(21,34);
                         adaptor.addChild(root_1, stream_c.nextNode());
 
                         adaptor.addChild(root_0, root_1);
@@ -286,34 +382,42 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                     }
                     break;
                 case 2 :
+                    dbg.enterAlt(2);
+
                     // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:22:3: ^( EQUALS ^( MINUS a= LETTER b= LETTER ) c= LETTER )
                     {
+                    dbg.location(22,3);
                     _last = (CommonTree)input.LT(1);
                     {
                     CommonTree _save_last_1 = _last;
                     CommonTree _first_1 = null;
-                    CommonTree root_1 = (CommonTree)adaptor.nil();_last = (CommonTree)input.LT(1);
+                    CommonTree root_1 = (CommonTree)adaptor.nil();dbg.location(22,5);
+                    _last = (CommonTree)input.LT(1);
                     EQUALS4=(CommonTree)match(input,EQUALS,FOLLOW_EQUALS_in_redraw119);  
                     stream_EQUALS.add(EQUALS4);
 
 
 
                     match(input, Token.DOWN, null); 
+                    dbg.location(22,12);
                     _last = (CommonTree)input.LT(1);
                     {
                     CommonTree _save_last_2 = _last;
                     CommonTree _first_2 = null;
-                    CommonTree root_2 = (CommonTree)adaptor.nil();_last = (CommonTree)input.LT(1);
+                    CommonTree root_2 = (CommonTree)adaptor.nil();dbg.location(22,14);
+                    _last = (CommonTree)input.LT(1);
                     MINUS5=(CommonTree)match(input,MINUS,FOLLOW_MINUS_in_redraw122);  
                     stream_MINUS.add(MINUS5);
 
 
 
                     match(input, Token.DOWN, null); 
+                    dbg.location(22,21);
                     _last = (CommonTree)input.LT(1);
                     a=(CommonTree)match(input,LETTER,FOLLOW_LETTER_in_redraw126);  
                     stream_LETTER.add(a);
 
+                    dbg.location(22,30);
                     _last = (CommonTree)input.LT(1);
                     b=(CommonTree)match(input,LETTER,FOLLOW_LETTER_in_redraw130);  
                     stream_LETTER.add(b);
@@ -322,6 +426,7 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                     match(input, Token.UP, null); adaptor.addChild(root_1, root_2);_last = _save_last_2;
                     }
 
+                    dbg.location(22,40);
                     _last = (CommonTree)input.LT(1);
                     c=(CommonTree)match(input,LETTER,FOLLOW_LETTER_in_redraw135);  
                     stream_LETTER.add(c);
@@ -333,7 +438,7 @@ public class SymbolraetselASTNormalizer extends TreeParser {
 
 
                     // AST REWRITE
-                    // elements: c, a, EQUALS, b
+                    // elements: c, EQUALS, a, b
                     // token labels: b, c, a
                     // rule labels: retval
                     // token list labels: 
@@ -348,21 +453,28 @@ public class SymbolraetselASTNormalizer extends TreeParser {
                     root_0 = (CommonTree)adaptor.nil();
                     // 23:3: -> ^( EQUALS ^( ADDITION $c $b) $a)
                     {
+                        dbg.location(23,6);
                         // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:23:6: ^( EQUALS ^( ADDITION $c $b) $a)
                         {
                         CommonTree root_1 = (CommonTree)adaptor.nil();
+                        dbg.location(23,8);
                         root_1 = (CommonTree)adaptor.becomeRoot(stream_EQUALS.nextNode(), root_1);
 
+                        dbg.location(23,15);
                         // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:23:15: ^( ADDITION $c $b)
                         {
                         CommonTree root_2 = (CommonTree)adaptor.nil();
+                        dbg.location(23,17);
                         root_2 = (CommonTree)adaptor.becomeRoot((CommonTree)adaptor.create(ADDITION, "ADDITION"), root_2);
 
+                        dbg.location(23,26);
                         adaptor.addChild(root_2, stream_c.nextNode());
+                        dbg.location(23,29);
                         adaptor.addChild(root_2, stream_b.nextNode());
 
                         adaptor.addChild(root_1, root_2);
                         }
+                        dbg.location(23,33);
                         adaptor.addChild(root_1, stream_a.nextNode());
 
                         adaptor.addChild(root_0, root_1);
@@ -384,6 +496,15 @@ public class SymbolraetselASTNormalizer extends TreeParser {
         }
         finally {
         }
+        dbg.location(23, 36);
+
+        }
+        finally {
+            dbg.exitRule(getGrammarFileName(), "redraw");
+            decRuleLevel();
+            if ( getRuleLevel()==0 ) {dbg.terminate();}
+        }
+
         return retval;
     }
     // $ANTLR end "redraw"
@@ -408,12 +529,20 @@ public class SymbolraetselASTNormalizer extends TreeParser {
 
         CommonTree set6_tree=null;
 
+        try { dbg.enterRule(getGrammarFileName(), "arith");
+        if ( getRuleLevel()==0 ) {dbg.commence();}
+        incRuleLevel();
+        dbg.location(25, 1);
+
         try {
             // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:25:8: ( PLUS | MINUS )
+            dbg.enterAlt(1);
+
             // C:\\Users\\Florian\\Dropbox\\Git\\CI_WiSe2013-2014\\CI_Aufgabe3\\teil1\\symbolraetsel_AST_Normalisiert\\SymbolraetselASTNormalizer.g:
             {
             root_0 = (CommonTree)adaptor.nil();
 
+            dbg.location(25,8);
             _last = (CommonTree)input.LT(1);
             set6=(CommonTree)input.LT(1);
             if ( (input.LA(1)>=PLUS && input.LA(1)<=MINUS) ) {
@@ -427,6 +556,7 @@ public class SymbolraetselASTNormalizer extends TreeParser {
             }
             else {
                 MismatchedSetException mse = new MismatchedSetException(null,input);
+                dbg.recognitionException(mse);
                 throw mse;
             }
 
@@ -443,6 +573,15 @@ public class SymbolraetselASTNormalizer extends TreeParser {
         }
         finally {
         }
+        dbg.location(25, 22);
+
+        }
+        finally {
+            dbg.exitRule(getGrammarFileName(), "arith");
+            decRuleLevel();
+            if ( getRuleLevel()==0 ) {dbg.terminate();}
+        }
+
         return retval;
     }
     // $ANTLR end "arith"
--- CI_Aufgabe3/teil1/symbolraetsel_AST_Normalisiert/run.java
+++ CI_Aufgabe3/teil1/symbolraetsel_AST_Normalisiert/run.java
@@ -11,18 +11,23 @@ import symbolraetsel_AST_Normalisiert.output.*;
 public class run {
 	public static void main(String[] args) {
 		try {
-			//Following according to: http://www.antlr.org/wiki/display/ANTLR3/Simple+tree-based+interpeter
-			
-			ANTLRFileStream input = new ANTLRFileStream("teil1/symbolraetsel_AST_Normalisiert/input.txt");
-	        SymbolraetselASTLexer lexer = new SymbolraetselASTLexer(input);
-	        CommonTokenStream tokens = new CommonTokenStream(lexer);
-	        SymbolraetselASTParser parser = new SymbolraetselASTParser(tokens);
-	        CommonTree t  = (CommonTree) parser.start().getTree(); //t = nuil ... pourquoi?????
+			// Following according to:
+			// http://www.antlr.org/wiki/display/ANTLR3/Simple+tree-based+interpeter
 
-	        CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
-	        SymbolraetselASTNormalizer normalizer = new SymbolraetselASTNormalizer(nodes);
-	        normalizer.start();
-			
+			ANTLRFileStream input = new ANTLRFileStream("teil1/symbolraetsel_AST_Normalisiert/input.txt");
+			SymbolraetselASTLexer lexer = new SymbolraetselASTLexer(input);
+			TokenRewriteStream tokens = new TokenRewriteStream(lexer);
+			SymbolraetselASTParser parser = new SymbolraetselASTParser(tokens);
+			SymbolraetselASTParser.start_return r1 = parser.start();
+			CommonTree t1 = (CommonTree) r1.getTree(); 
+			//CommonTree t1  = (CommonTree) parser.prog().getTree();
+			System.out.println(t1.toStringTree());
+
+			CommonTreeNodeStream nodes = new CommonTreeNodeStream(t1);
+			SymbolraetselASTNormalizer normalizer = new SymbolraetselASTNormalizer(nodes);
+			SymbolraetselASTNormalizer.start_return r2 = normalizer.start();
+			CommonTree t2 = (CommonTree) r2.getTree();
+			System.out.println(t2.toStringTree());
 
 		} catch (IOException e) {
 			e.printStackTrace();
